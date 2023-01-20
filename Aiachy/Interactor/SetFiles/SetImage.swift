543cfd6a8708f157983a88531406f0b351fd3518
NomoteteS <nomotetes.onetrue@icloud.com>
2023-01-20 22:14:33 +0200

    SetFiles
    Images pre-made for onboarding

---
+++ Aiachy/Interactor/SetFiles/SetImage.swift
@@ -0,0 +1,29 @@
+//
+//  SetImage.swift
+//  Aiachy
+//
+//  Created by NomoteteS on 20.01.2023.
+//
+
+import Foundation
+
+struct SetImage {
+    
+    enum SplashView : String {
+        case SplashTopVector = "SplashTopVector"
+        case SplashBottomVector = "SplashBottomVector"
+    }
+    
+    enum Onboarding : String {
+        case BackgroundStars = "BackgroundStars"
+        case ONBFirstVector = "ONBFirstPageVector"
+        case ONBFirst = "ONBFirst"
+        case ONBLastVector = "ONBLastVector"
+        case ONBLast = "ONBLast"
+        case WelcomeVector = "WelcomeVector"
+        case Welcome = "Welcome"
+        case LoginVector = "LoginVector"
+        case Login = "Login"
+        case AttentionBird = "AttentionBird"
+    }
+}
